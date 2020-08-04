90e01e32e17c38087261a1889a93967b682360d1
vikrams <vk504376@dal.ca>
2020-08-03 21:11:13 -0300

    Added Notifications to whitelist for development

--- jwt.js
+++ jwt.js
@@ -9,10 +9,10 @@ function jwt() {
     path: [
       // public routes that don't require authentication
       "/api/faqs/getAllFaqs",
-      "/api/notifications/getAllNotifications",
       "/api/users/authenticate",
       "/api/users/add-user",
       "/api/properties/allProperties",
+      new RegExp('/api/notifications/.*', 'i'),
       new RegExp('/api/properties/allNearByProperties.*', 'i'),
       new RegExp('/api/properties/filterallNearByProperties.*', 'i'),
     ],
